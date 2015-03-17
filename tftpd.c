e166b5e443e3ba0204fd1ef4255f661f45bf6b0f
Scott Anderson <ascent12@hotmail.com>
2015-03-17 15:16:51 +1300

    Added file finding

--- tftpd.c
+++ tftpd.c
@@ -1,8 +1,12 @@
+#include <ctype.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include <dirent.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -10,6 +14,53 @@
 #include "network.h"
 
 #define PACKET_SIZE 512
+
+bool validate_filename(const char *filename)
+{
+	if (strcmp(filename, ".") == 0 || strcmp(filename, "..") == 0)
+		return false;
+
+	for (const char *ptr = filename; *ptr != '\0'; ++ptr)
+		if (!isalpha(*ptr) && *ptr != '.')
+			return false;
+
+	return true;
+}
+
+int find_file(const char *filename)
+{
+	int fd;
+	DIR *tftp;
+	struct dirent *d;
+
+	tftp = opendir("./Tftp");
+	if (tftp == NULL) {
+		fprintf(stderr, "Unable to open tftp directory: %s\n",
+				strerror(errno));
+		return -1;
+	}
+
+	while ((errno = 0) || (d = readdir(tftp)) != NULL) {
+		if (strcmp(d->d_name, filename) == 0) {
+			fd = openat(dirfd(tftp), d->d_name, O_RDONLY);
+			if (fd == -1)
+				fprintf(stderr, "Could not open %s: %s\n",
+						d->d_name, strerror(errno));
+
+			errno = 0;
+			break;
+		}
+	}
+
+	if (errno != 0) {
+		fprintf(stderr, "Unable to read tftp directory: %s\n",
+				strerror(errno));
+	}
+
+	closedir(tftp);
+
+	return fd;
+}
 
 int wait_connection(int srv_sock, char *buffer, ssize_t *nread)
 {
@@ -43,11 +94,14 @@ int wait_connection(int srv_sock, char *buffer, ssize_t *nread)
 int main(int argc, char *argv[])
 {
 	int srv_sock;
+	int fd;
 
 	if (argc > 2) {
 		fprintf(stderr, "Usage: %s [port]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+
+	fd = find_file("hello");
 
 	srv_sock = open_socket(NULL, argc == 2 ? argv[1] : "69",
 			AF_INET6, AI_PASSIVE | AI_V4MAPPED, bind);
@@ -66,5 +120,6 @@ int main(int argc, char *argv[])
 		close(client_sock);
 	}
 
+	close(fd);
 	close(srv_sock);
 }
